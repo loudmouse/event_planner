ffa533fa426d3671deff2bb2740a91bcc13f2795
Nolan Neeley <nolanneeley@gmail.com>
2018-04-12 10:18:16 -0500

    generated scaffold for the event object

---
+++ db/migrate/20180412151311_create_events.rb
@@ -0,0 +1,12 @@
+class CreateEvents < ActiveRecord::Migration[5.1]
+  def change
+    create_table :events do |t|
+      t.text :description
+      t.string :location
+      t.date :date
+      t.time :time
+
+      t.timestamps
+    end
+  end
+end
